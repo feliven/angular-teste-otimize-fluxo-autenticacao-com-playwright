8e1e2cdd9ca31d0791be28597fb7708080e3cdc1
FVS <feliven@gmail.com>
2025-12-05 11:52:58 -0300

    implementa teste completo para página de cadastro

--- e2e/page-objects/PaginaCadastro.ts
+++ e2e/page-objects/PaginaCadastro.ts
@@ -36,7 +36,9 @@ export default class PaginaCadastro {
     );
     this.campoTelefone = page.getByTestId('form-base-input-telefone');
     this.campoCidade = page.getByTestId('form-base-input-cidade');
-    this.campoEstado = page.getByTestId('form-base-container-estado');
+    this.campoEstado = page
+      .getByTestId('form-base-container-estado')
+      .getByLabel('Estado');
     this.campoEmail = page.getByTestId('form-base-input-email');
     this.campoSenha = page.getByTestId('form-base-input-senha');
     this.campoConfirmarEmail = page.getByTestId(
@@ -55,6 +57,81 @@ export default class PaginaCadastro {
     await this.page.goto('/');
     await this.botaoVisitarPaginaCadastro.click();
     await expect(this.page).toHaveURL('/auth/cadastro');
+  }
+
+  async inserirNome(nome: string) {
+    await this.campoNome.fill(nome);
+  }
+
+  async inserirDataNascimento(dataFormatoBR: Date) {
+    const options: Intl.DateTimeFormatOptions = {
+      month: '2-digit',
+      day: '2-digit',
+      year: '2-digit',
+    };
+
+    const dataFormatoUS = dataFormatoBR.toLocaleDateString('en-US', options);
+
+    await this.campoDataNascimento.fill(dataFormatoUS);
+  }
+
+  async selecionarGenero(opcao: string) {
+    switch (opcao) {
+      case 'FEMININO':
+        await this.radioFeminino.click();
+        break;
+
+      case 'MASCULINO':
+        await this.radioMasculino.click();
+        break;
+
+      case 'OUTRO':
+        await this.radioNaoInformar.click();
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  async inserirCPF(cpf: string) {
+    await this.campoCPF.fill(cpf);
+  }
+
+  async inserirTelefone(telefone: string) {
+    await this.campoTelefone.fill(telefone);
+  }
+
+  async inserirCidade(cidade: string) {
+    await this.campoCidade.fill(cidade);
+  }
+
+  async inserirEstado(estado: string) {
+    await this.campoEstado.fill(estado);
+  }
+
+  async inserirEmail(email: string) {
+    await this.campoEmail.fill(email);
+  }
+
+  async inserirSenha(senha: string) {
+    await this.campoSenha.fill(senha);
+  }
+
+  async inserirConfirmacaoEmail(email: string) {
+    await this.campoConfirmarEmail.fill(email);
+  }
+
+  async inserirConfirmacaoSenha(senha: string) {
+    await this.campoConfirmarSenha.fill(senha);
+  }
+
+  async marcarCheckboxAceite() {
+    await this.checkboxTermos.click();
+  }
+
+  async clicarBotaoCadastrar() {
+    await this.botaoEnviarFormulario.click();
   }
 
   async estaMostrandoMensagemDeErro(mensagem: string) {
--- e2e/specs/cadastro.spec.ts
+++ e2e/specs/cadastro.spec.ts
@@ -1,6 +1,5 @@
 import { gerarPerfil } from 'e2e/operacoes/gerarPerfil';
 import { test } from '../setup/fixtures';
-import { log } from 'console';
 
 test.describe('Página de cadastro', () => {
   test('Deve conseguir fazer cadastro', async ({ paginaCadastro }) => {
@@ -9,5 +8,19 @@ test.describe('Página de cadastro', () => {
     const novoUsuario = gerarPerfil();
 
     console.log(novoUsuario);
+
+    await paginaCadastro.inserirNome(novoUsuario.nome);
+    await paginaCadastro.inserirDataNascimento(novoUsuario.dataNascimento);
+    await paginaCadastro.selecionarGenero(novoUsuario.genero);
+    await paginaCadastro.inserirCPF(novoUsuario.cpf);
+    await paginaCadastro.inserirTelefone(novoUsuario.telefone);
+    await paginaCadastro.inserirCidade(novoUsuario.cidade);
+    await paginaCadastro.inserirEstado(novoUsuario.estado);
+    await paginaCadastro.inserirEmail(novoUsuario.email);
+    await paginaCadastro.inserirSenha(novoUsuario.senha);
+    await paginaCadastro.inserirConfirmacaoEmail(novoUsuario.email);
+    await paginaCadastro.inserirConfirmacaoSenha(novoUsuario.senha);
+    await paginaCadastro.marcarCheckboxAceite();
+    await paginaCadastro.clicarBotaoCadastrar();
   });
 });
