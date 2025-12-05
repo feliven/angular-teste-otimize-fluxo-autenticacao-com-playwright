8038df64fb28b88cd4b6a1489ae17331039a988f
FVS <feliven@gmail.com>
2025-12-05 10:19:47 -0300

    cria teste para cadastro e page-object PaginaCadastro

---
+++ e2e/page-objects/PaginaCadastro.ts
@@ -0,0 +1,60 @@
+import { Locator, Page, expect } from '@playwright/test';
+
+export default class PaginaCadastro {
+  private readonly page: Page;
+  private readonly campoDataNascimento: Locator;
+  private readonly campoNome: Locator;
+  private readonly campoCPF: Locator;
+  private readonly radioFeminino: Locator;
+  private readonly radioMasculino: Locator;
+  private readonly radioNaoInformar: Locator;
+  private readonly campoTelefone: Locator;
+  private readonly campoCidade: Locator;
+  private readonly campoEstado: Locator;
+  private readonly campoEmail: Locator;
+  private readonly campoSenha: Locator;
+  private readonly campoConfirmarEmail: Locator;
+  private readonly campoConfirmarSenha: Locator;
+  private readonly checkboxTermos: Locator;
+  private readonly botaoEnviarFormulario: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.campoNome = page.getByTestId('form-base-input-nome');
+    this.campoDataNascimento = page.getByTestId(
+      'form-base-input-data-nascimento'
+    );
+    this.campoCPF = page.getByTestId('form-base-input-cpf');
+    this.radioFeminino = page.getByTestId('form-base-radio-genero-feminino');
+    this.radioMasculino = page.getByTestId('form-base-radio-genero-masculino');
+    this.radioNaoInformar = page.getByTestId(
+      'form-base-radio-genero-nao-informar'
+    );
+    this.campoTelefone = page.getByTestId('form-base-input-telefone');
+    this.campoCidade = page.getByTestId('form-base-input-cidade');
+    this.campoEstado = page.getByTestId('form-base-container-estado');
+    this.campoEmail = page.getByTestId('form-base-input-email');
+    this.campoSenha = page.getByTestId('form-base-input-senha');
+    this.campoConfirmarEmail = page.getByTestId(
+      'form-base-input-confirmar-email'
+    );
+    this.campoConfirmarSenha = page.getByTestId(
+      'form-base-input-confirmar-senha'
+    );
+    this.checkboxTermos = page.getByTestId('form-base-checkbox-termos');
+    this.botaoEnviarFormulario = page.getByTestId(
+      'form-base-botao-submeter-form'
+    );
+  }
+
+  async visitar() {
+    await this.page.goto('/');
+    // await this.botaoLogin.click();
+    // await expect(this.page).toHaveURL('/auth/login');
+  }
+
+  async estaMostrandoMensagemDeErro(mensagem: string) {
+    const elementoErro = this.page.getByText(mensagem);
+    await expect(elementoErro).toBeVisible();
+  }
+}
--- e2e/setup/fixtures.ts
+++ e2e/setup/fixtures.ts
@@ -1,10 +1,12 @@
 import { test as base } from '@playwright/test';
-import PaginaLogin from 'e2e/page-objects/PaginaLogin';
-import PaginaPrincipal from 'e2e/page-objects/PaginaPrincipal';
+import PaginaCadastro from '../page-objects/PaginaCadastro';
+import PaginaLogin from '../page-objects/PaginaLogin';
+import PaginaPrincipal from '../page-objects/PaginaPrincipal';
 
 export const test = base.extend<{
   paginaLogin: PaginaLogin;
   paginaPrincipal: PaginaPrincipal;
+  paginaCadastro: PaginaCadastro;
 }>({
   paginaLogin: async ({ page }, use) => {
     const paginaLogin = new PaginaLogin(page);
@@ -16,5 +18,11 @@ export const test = base.extend<{
     const paginaPrincipal = new PaginaPrincipal(page);
     await paginaPrincipal.visitar();
     await use(paginaPrincipal);
+  },
+
+  paginaCadastro: async ({ page }, use) => {
+    const paginaCadastro = new PaginaCadastro(page);
+    await paginaCadastro.visitar();
+    await use(paginaCadastro);
   },
 });
