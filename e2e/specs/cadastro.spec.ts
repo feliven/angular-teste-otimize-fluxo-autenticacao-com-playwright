99edea7c9c5678f0a966e5f01d39e0fd11be2982
FVS <feliven@gmail.com>
2025-12-05 11:20:47 -0300

    instala e implementa faker-js

--- e2e/specs/cadastro.spec.ts
+++ e2e/specs/cadastro.spec.ts
@@ -1,7 +1,13 @@
+import { gerarPerfil } from 'e2e/operacoes/gerarPerfil';
 import { test } from '../setup/fixtures';
+import { log } from 'console';
 
 test.describe('Página de cadastro', () => {
   test('Deve conseguir fazer cadastro', async ({ paginaCadastro }) => {
     await paginaCadastro.visitar();
+
+    const novoUsuario = gerarPerfil();
+
+    console.log(novoUsuario);
   });
 });
